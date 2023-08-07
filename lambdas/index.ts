0d9d89e64a77a9de862030b76a94441e35ae04fc
Masoud Rousta <masoud.rousta@vyprco.com>
2023-08-07 16:15:58 +0100

    fix: fix logic

--- lambdas/index.ts
+++ lambdas/index.ts
@@ -4,8 +4,7 @@ import { customerUserService } from '../services/customerUserService';
 exports.handler = async function (event: any) {
     const { httpMethod, path } = event;
     const splitPath = path.split('/');
-    if (splitPath[1] && splitPath[1] === 'user')
-        return customerUserService(httpMethod, event);
+    if (splitPath[1] === 'user') return customerUserService(httpMethod, event);
     else if (splitPath[1] !== 'user' && splitPath[0] === 'customer')
         return customerService(httpMethod, event);
     else return { statusCode: 400, body: 'invalid path' };
